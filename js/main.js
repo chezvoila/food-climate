ac9737f1b1b5bed1bf2dfb6782ba18f240a1c4f2
Maxime <maxime.pub1@gmail.com>
2020-06-12 18:23:00 +0200

    globalwarming

--- js/main.js
+++ js/main.js
@@ -1,28 +1,51 @@
 (function (d3) {
 
-        /***** DOM *****/
+    /***** DOM *****/
+
+
+    /* GLOBAL WARMING */
+
+    var elements = create_GW(d3);
+    var svg_GW = elements.svg,
+        x_GW = elements.x,
+        y_GW = elements.y;
+
+    /***** bind events *****/
 
 
 
-        /***** bind events *****/
+    /***** Loading data *****/
+    var promises = [];
+    promises.push(d3.csv("./data/foodco2.csv"));
+    // promises.push(d3.csv("./data/globalwarming.csv"));
+    promises.push(d3.csv("./data/globalwarming_emissions.csv"));
+    promises.push(d3.csv("./data/globalwarming_temperature.csv"));
+    // promises.push(d3.csv("./data/chainco2.csv"));
+    // promises.push(d3.csv("./data/land.csv"));
+
+
+    Promise.all(promises)
+        /***** functions *****/
+        .then(function (data) {
+            foodco2(data[0]);
+            const data_GW = globalwarming(data[1], data[2]);
+            // chainco2(data[2]);
+            // land(data[3]);
 
 
 
-        /***** Loading data *****/
-        var promises = [];
-        promises.push(d3.csv("./data/foodco2.csv"));
-        // promises.push(d3.csv("./data/globalwarming.csv"));
-        // promises.push(d3.csv("./data/chainco2.csv"));
-        // promises.push(d3.csv("./data/land.csv"));
+            /* GLOBAL WARMING */
+
+            // update domains
+            xDomain_GW(d3, x_GW, data_GW);
+            yDomain_GW(d3, y_GW, data_GW);
+
+            // create and display the chart
+            chart_GW(svg_GW, x_GW, y_GW, data_GW);
 
 
-        Promise.all(promises)
-                /***** functions *****/
-                .then(function (data) {
-                        foodco2(data[0]);
-                        globalwarming(data[1]);
-                        chainco2(data[2]);
-                        land(data[3]);
+        });
 
-                });
+
+
 })(d3);
