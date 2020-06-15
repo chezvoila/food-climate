6eb36460535c66c50ac2adbc339684030e48145c
ikamyx <ikammmy@gmail.com>
2020-06-15 13:58:08 -0400

    pull01

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
+"use strict";
+
 (function (d3) {
+        /***** settings *****/
+        let _ = {
+                colors: {
+                        dark3: "#160508",
+                        dark2: "#581420",
+                        dark1: "#992437",
+                        main: "#db334f",
+                        light1: "#e67084",
+                        light2: "#f1adb9",
+                        light3: "#fbebed",
+                        icon_bg: "#a8243a",
+                        icon_hover: "#6b101f"
+                }
+        }
+
 
         /***** DOM *****/
 
@@ -19,10 +36,10 @@
         Promise.all(promises)
                 /***** functions *****/
                 .then(function (data) {
-                        foodco2(data[0]);
-                        globalwarming(data[1]);
-                        chainco2(data[2]);
-                        land(data[3]);
+                        foodco2(data[0], _);
+                        // globalwarming(data[1]);
+                        // chainco2(data[2]);
+                        // land(data[3]);
 
                 });
 })(d3);
