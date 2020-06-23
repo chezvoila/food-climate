c335124e0d4561ffad16e35fc8631f8024c1b420
Maxime <maxime.pub1@gmail.com>
2020-06-23 17:46:04 +0200

    land code with scoll nothing in main about land

--- js/main.js
+++ js/main.js
@@ -26,9 +26,6 @@
         x_GW = elements.x,
         y_GW = elements.y;
 
-    /* LAND */
-
-    var color = d3.scaleOrdinal(d3.schemePaired).range(['#ffffff', '#992437']); // TO DEFINE
 
     /***** bind events *****/
 
@@ -71,66 +68,6 @@
             chart_GW(svg_GW, x_GW, y_GW, data_GW);
 
 
-            /* LAND */
-
-            // Get different sets of data
-            const data_consumption = data_land.consumption;
-            const data_intake = data_land.intake;
-
-            // Make a list of the countries that are in the 4 datasets
-            const lands_available = make_list_land(data_consumption, data_intake);
-            lands_available.push("World")
-
-            // update color domain
-            land_color_domain(color, lands_available);
-
-            // default display is Canada
-            var country = "Canada";
-            // get areas
-            const defaultArea = 100000;
-            var area_country = getAreaCountry(defaultArea, data_consumption, country);
-            // append (so first = true) and display the 2 charts
-            var svg_world = init_division_world(defaultArea, color, true);
-            var svg_country = init_division_country(area_country, color, true, country);
-            // transitions on mouseover and mouseout
-            d3.select("#land_charts")
-                .on("click", _ => {
-                    chart_division_world(svg_world, data_intake, defaultArea);
-                    chart_division_country(svg_country, data_intake, defaultArea, country, color);
-                })
-            // .on("mouseout", _ => {
-            //     init_division_world(defaultArea, color, false, svg_world);
-            //     init_division_country(area_country, color, false, country, svg_country);
-            // })
-
-            // Autocomplete for the search bar
-            new autoComplete({
-                selector: "#search-bar input",
-                minChars: 1,
-                source: function (term, suggest) {
-                    term = term.toLowerCase();
-                    var matches = [];
-                    lands_available.forEach(function (d) {
-                        if (~d.toLowerCase().indexOf(term)) {
-                            matches.push(d);
-                        }
-                    });
-                    suggest(matches);
-                },
-                renderItem: function (item, search) {
-                    search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
-                    var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
-                    return '<div class="autocomplete-suggestion" data-val="'
-                        + item + '">' + item.replace(re, "<b>$1</b>") + "</div>";
-                },
-                onSelect: function (e, term, item) {
-                    country = term;
-                    area_country = getAreaCountry(defaultArea, data_consumption, country);
-                    init_division_country(area_country, color, false, country, svg_country);
-                    init_division_world(defaultArea, color, false, svg_world);
-                    d3.select("#div_columns").classed('display', false)
-                }
-            });
         });
 
 
@@ -285,7 +222,7 @@
         let element = el.querySelector("section");
         if (dir) {
             total = el.scrollTop + el.clientHeight;
-            if ((total >= element.clientHeight) && (index < 4) && flag) {
+            if ((total >= element.clientHeight - 1) && (index < 4) && flag) {
                 jumpNext(el.nextElementSibling);
             }
         } else {
