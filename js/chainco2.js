6025dec5fa9fb8afdbc0deb9284ae7eeb6122fd6
Adam Michaud <adam13@hotmail.ca>
2020-06-24 11:59:58 -0400

    finished the scrollbar and continued animations on chainco2

--- js/chainco2.js
+++ js/chainco2.js
@@ -8,51 +8,61 @@
  */
 function chainco2(data) {
     //Define the size of the svg element
-    let aspectRatioHeight = (1080 / 1920) * document.documentElement.clientWidth;
+    //console.log(document.documentElement.clientWidth, document.documentElement.clientHeight)
+    let aspectRatioHeight = (969 / 1920) * document.documentElement.clientWidth;
 
-    let sizeSettings = { width: document.documentElement.clientWidth,
-                         height: aspectRatioHeight }
-    
+    let sizeSettings = {
+        width: document.documentElement.clientWidth,
+        height: aspectRatioHeight
+    }
+
     //Initialize the svg element
-    let svg = d3.select("#chain_co2").append("svg")
-    .attr("viewBox", `0 0 ${sizeSettings.width} ${sizeSettings.height}`)
+    let svg = d3.select("#chain_co2 .chart").append("svg")
+        .attr("viewBox", `0 0 ${sizeSettings.width} ${sizeSettings.height}`)
 
     //define the settings for the donut chart. It will auto-adjust
-    const donutSettings = {position: { x: sizeSettings.width / 2, 
-                                y: 0.95*sizeSettings.height },
-                    radius: sizeSettings.width / 5 ,
-                    thickness: sizeSettings.width / 15 }  
-    
+    const donutSettings = {
+        position: {
+            x: sizeSettings.width / 2,
+            y: 0.95 * sizeSettings.height
+        },
+        radius: sizeSettings.width / 5,
+        thickness: sizeSettings.width / 15
+    }
+
     //draw the highlighted background when second level is discovered
     drawScope(svg, donutSettings, data)
-    
+
     //draw the visualization title
-    const title = {text: "The food CO2 emissions",
-                   position: { x: sizeSettings.width/2, 
-                               y: sizeSettings.height/5 }
-                  }
-    
+    const title = {
+        text: "The food CO2 emissions",
+        position: {
+            x: sizeSettings.width / 2,
+            y: sizeSettings.height / 5
+        }
+    }
+
     //define tooltip
     let tip = d3.tip()
-                .attr("class", "d3-tip")
-                .html(function(d) { return d.value*100+"%"; });
+        .attr("class", "d3-tip")
+        .html(function (d) { return d.value * 100 + "%"; });
 
     //define the group that will be used to draw the donut chart
     let g = svg.append("g")
-                .attr("transform", `translate(${donutSettings.position.x}, ${donutSettings.position.y})`)
-                .call(tip)
-    
+        .attr("transform", `translate(${donutSettings.position.x}, ${donutSettings.position.y})`)
+        .call(tip)
+
     //draw level 1 with specified angles settings
     const anglesLevel1 = {
-        startAngle: -Math.PI/2,
-        endAngle: Math.PI/2
+        startAngle: -Math.PI / 2,
+        endAngle: Math.PI / 2
     }
     plotLevel(g, donutSettings, anglesLevel1, 1, data.level1, tip)
-    
+
     //draw level 2 with specified angles settings
     const anglesLevel2 = {
-        startAngle: -Math.PI/2,
-        endAngle: -Math.PI/2+Math.PI*data.level1[0].v
+        startAngle: -Math.PI / 2,
+        endAngle: -Math.PI / 2 + Math.PI * data.level1[0].v
     }
     plotLevel(g, donutSettings, anglesLevel2, 2, data.level2, tip)
 
@@ -71,61 +81,61 @@ function chainco2(data) {
  * @param tip             The D3 tooltip to display information
  *
  */
-function plotLevel(g, settings, angles, level, data, tip){
-    
+function plotLevel(g, settings, angles, level, data, tip) {
+
     //Define the color scale
     let color = d3.scaleOrdinal();
-    color.domain(data.map((_,i) => i))
+    color.domain(data.map((_, i) => i))
     color.range(data.map(d => d.color))
 
     //d3 define pie function for angles calculation
     let pies = d3.pie()
-    	         .value( d => d)
-                 .sort(null)
-                 .startAngle(angles.startAngle)
-                 .endAngle( angles.endAngle)
-    
+        .value(d => d)
+        .sort(null)
+        .startAngle(angles.startAngle)
+        .endAngle(angles.endAngle)
+
     //d3 define arc function
-    const radius = level > 1 ? settings.radius*(1.8*(level-1)) : settings.radius;
-	let arc = d3.arc()
-    	        .outerRadius(radius)
-                .innerRadius(radius - settings.thickness)
+    const radius = level > 1 ? settings.radius * (1.8 * (level - 1)) : settings.radius;
+    let arc = d3.arc()
+        .outerRadius(radius)
+        .innerRadius(radius - settings.thickness)
 
     //define groups to draw the text and the path inside
     let groups = g.selectAll(`g.level${level}`)
-                  .data(pies(data.map(d => d.v)))
-                  .enter()
-                  .append("g")
-                  .classed(`level${level}`, true)
+        .data(pies(data.map(d => d.v)))
+        .enter()
+        .append("g")
+        .classed(`level${level}`, true)
 
     //draw the arcs on the donut chart
     groups.append("path")
-          .attr("id", (_,i) => `level${level}_${i}`)
-          .attr("fill", d => color(d.index))
-          .attr("d", arc)
-          .style("opacity", 0.85)
-          .on('mouseover', function(d, i){
-              tip.show(d, this);
-              d3.select(this).style("opacity", 100)
-          }
-            )
-          .on('mouseout', function(d, i){
+        .attr("id", (_, i) => `level${level}_${i}`)
+        .attr("fill", d => color(d.index))
+        .attr("d", arc)
+        .style("opacity", 0.85)
+        .on('mouseover', function (d, i) {
+            tip.show(d, this);
+            d3.select(this).style("opacity", 100)
+        }
+        )
+        .on('mouseout', function (d, i) {
             tip.hide(d, this);
             d3.select(this).style("opacity", 0.85)
         })
 
     //add the text on the arc
     groups.selectAll("text")
-          .data(data)
-          .enter()
-          .append("text")
-          .attr("dx", 20)
-          .attr("dy", -15)
-          .append("textPath")
-          .classed("text", true)
-          .attr("xlink:href", (_,i) => `#level${level}_${i}`) 
-          .text(d => d.name)
-          .classed("donutLabel", true)
+        .data(data)
+        .enter()
+        .append("text")
+        .attr("dx", 20)
+        .attr("dy", -15)
+        .append("textPath")
+        .classed("text", true)
+        .attr("xlink:href", (_, i) => `#level${level}_${i}`)
+        .text(d => d.name)
+        .classed("donutLabel", true)
 }
 
 /**
@@ -136,36 +146,38 @@ function plotLevel(g, settings, angles, level, data, tip){
  * @param data            The data to compute angles.
  *
  */
-function drawScope(svg, settings, data){
+function drawScope(svg, settings, data) {
     //compute the angle of the donut chart
-    const tanAngle = Math.tan(Math.PI*data.level1[0].v);
+    const tanAngle = Math.tan(Math.PI * data.level1[0].v);
 
     //define the points and the path command associated with
     const points = [
         {
-            cmd: "M", 
-            x: 0, 
-            y: settings.position.y - tanAngle*settings.position.x
+            cmd: "M",
+            x: 0,
+            y: settings.position.y - tanAngle * settings.position.x
         },
         {
-            cmd: "L", 
-            x: 0, 
-            y: settings.position.y},
-        {   cmd: "L", 
-            x: settings.position.x-settings.radius, 
+            cmd: "L",
+            x: 0,
             y: settings.position.y
         },
         {
-            cmd: "L", 
-            x: settings.position.x-settings.radius+settings.thickness, 
-            y: settings.position.y - tanAngle*(settings.radius-settings.thickness) 
+            cmd: "L",
+            x: settings.position.x - settings.radius,
+            y: settings.position.y
+        },
+        {
+            cmd: "L",
+            x: settings.position.x - settings.radius + settings.thickness,
+            y: settings.position.y - tanAngle * (settings.radius - settings.thickness)
         }
-   ]
+    ]
 
-   //draw the background using the specified points
+    //draw the background using the specified points
     svg.append("path")
-    .attr("d", points.reduce((path, actual) => path + `${actual.cmd}${actual.x}  ${actual.y}`, "") +"Z")
-    .classed("highlightedBackground", true)
+        .attr("d", points.reduce((path, actual) => path + `${actual.cmd}${actual.x}  ${actual.y}`, "") + "Z")
+        .classed("highlightedBackground", true)
 }
 
 /**
@@ -177,59 +189,130 @@ function drawScope(svg, settings, data){
  * @param data            The data to compute angles.
  *
  */
-function drawSeparatingLine(svg, settings, data){
-    const angle = Math.PI*data.level1[0].v*data.level2[0].v;
+let linePoints = []
+
+function drawSeparatingLine(svg, settings, data) {
+    const angle = Math.PI * data.level1[0].v * data.level2[0].v;
     const tanAngle = Math.tan(angle);
     const cosAngle = Math.cos(angle);
     const sinAngle = Math.sin(angle);
-    const linePoints = [
+    linePoints = [
         {
-            x: 0, 
-            y: settings.position.y - tanAngle*settings.position.x
+            x: 0,
+            y: settings.position.y - tanAngle * settings.position.x
         },
         {
-            x: settings.position.x-cosAngle*(settings.radius-settings.thickness), 
-            y: settings.position.y - sinAngle*(settings.radius-settings.thickness)}
-   ]
+            x: settings.position.x - cosAngle * (settings.radius - settings.thickness),
+            y: settings.position.y - sinAngle * (settings.radius - settings.thickness)
+        }
+    ]
 
     svg.append("line")
-       .attr("x1", linePoints[0].x)
-       .attr("y1", linePoints[0].y)
-       .attr("x2", linePoints[1].x)
-       .attr("y2", linePoints[1].y)
-       .attr("stroke-dasharray", 4)
-       .attr("stroke-width", 2)
-       .attr("stroke", "#6b101f")
-       .classed("chainCo2_line", true)
+        .attr("x1", linePoints[1].x)
+        .attr("y1", linePoints[1].y)
+        .attr("x2", linePoints[1].x)
+        .attr("y2", linePoints[1].y)
+        .attr("stroke-dasharray", 4)
+        .attr("stroke-width", 2)
+        .attr("stroke", "#6b101f")
+        .classed("chainCo2_line", true)
 }
 
 
 /********************* SCROLL ****************/
-let chainCo2_animated = 0;
 function chain_co2_scroll(position) {
-    console.log(position, "   ", chainCo2_animated)
-    if(position > 500 && chainCo2_animated < 500){
-        d3.select(".highlightedBackground")
-                    .transition()
-                    .duration(1000)
-                    .ease(d3.easeLinear)
-                    .style("opacity", 1);
+    console.log(position)
 
-                    d3.selectAll(".level2")
-                    .transition()
-                    .delay(1000)
-                    .duration(1000)
-                    .ease(d3.easeLinear)
-                    .style("opacity", 1);
-
-                    d3.selectAll(".chainCo2_line")
-                    .transition()
-                    .delay(2000)
-                    .duration(1000)
-                    .ease(d3.easeLinear)
-                    .style("opacity", 100);
+    //Title animations
+    if(position >= 0 && position < 700){
+        d3.select("#chain_co2 h1")
+          .classed("sticky", true)
+          .classed("fadein", true);
     }
-    if(chainCo2_animated < position){
-        chainCo2_animated = position;
+
+    if(position > 100 && position < 3500){
+        d3.select("#chain_co2 #chain_co2_text1")
+          .classed("sticky", true)
+          .classed("fadein", true)
+          .classed("fadeout", false);
+    }
+    else{
+        d3.select("#chain_co2 #chain_co2_text1")
+          .classed("fadeout", true)
+          .classed("fadein", false);
+    }
+
+    if(position > 3500){
+        d3.select("#chain_co2 #chain_co2_text2")
+          .classed("sticky", true)
+          .classed("fadein", true)
+          .classed("fadeout", false);
+    }
+    else{
+        d3.select("#chain_co2 #chain_co2_text2")
+          .classed("fadeout", true)
+          .classed("fadein", false);
+    }
+
+    //chart animations
+    if (position > 2200) {
+        d3.select("#chain_co2 .chart")
+          .classed("fadein", true)
+          .classed("fadeout", false);
+
+        d3.select("#chain_co2 svg")
+          .classed("sticky", true);
+
+          if(position > 3500){
+            d3.select(".highlightedBackground")
+                .transition()
+                .duration(1000)
+                .ease(d3.easeLinear)
+                .style("opacity", 1);
+
+            d3.selectAll(".level2")
+                .transition()
+                .delay(1000)
+                .duration(1000)
+                .ease(d3.easeLinear)
+                .style("opacity", 1);
+            if(position > 3900){
+                d3.selectAll(".chainCo2_line")
+                    .transition()
+                    .duration(1000)
+                    .ease(d3.easeLinear)
+                    .style("opacity", 100)
+                    .attr("x2", linePoints[0].x)
+                    .attr("y2", linePoints[0].y)
+            }
+            else{
+                d3.selectAll(".chainCo2_line")
+                    .transition()
+                    .duration(300)
+                    .ease(d3.easeLinear)
+                    .style("opacity", 0)
+                    .attr("x2", linePoints[1].x)
+                    .attr("y2", linePoints[1].y)
+            }
+          }
+          else{
+                d3.selectAll(".level2")
+                  .transition()
+                  .duration(300)
+                  .ease(d3.easeLinear)
+                  .style("opacity", 0);
+
+                d3.select(".highlightedBackground")
+                .transition()
+                .delay(300)
+                .duration(300)
+                .ease(d3.easeLinear)
+                .style("opacity", 0);
+          }
+    }
+    else{
+        d3.select("#chain_co2 .chart")
+          .classed("fadeout", true)
+          .classed("fadein", false);
     }
 }
